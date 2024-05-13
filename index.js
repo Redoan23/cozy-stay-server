504113f4c501cee71df00e3ee58d31af03cb15e9
Redoan23 <redoan1080@gmail.com>
2024-05-13 23:12:08 +0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
+const logger = (req, res, next) => {
+    // console.log('log-info:', req.method, req.url)
+    next()
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.Token
+    // console.log('the middleware token is', token)
+    if (!token) {
+        return res.status(401).send({ message: 'Access Unauthorized' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+
+        req.user = decoded
+        // console.log(decoded)
+        next()
+    })
+}
 
 
 
@@ -56,7 +85,13 @@ async function run() {
 
         // booking list management 
 
-        app.get('/booked/user', async (req, res) => {
+        app.get('/booked/user', logger, verifyToken, async (req, res) => {
+            console.log(req.cookies)
+            console.log('the token owner is', req.user)
+            console.log(req.query.email)
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: ' access denied' })
+            }
             const result = await usersBookedCollection.find().toArray()
             res.send(result)
         })
@@ -105,22 +140,48 @@ async function run() {
 
         // this is for the review collection
 
-        app.post('/user/review', async (req, res) => {
+        app.post('/user/review', verifyToken, async (req, res) => {
             const review = req.body.reviewData
             console.log(review)
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: ' access denied' })
+            }
             const result = await reviewCollection.insertOne(review)
             res.send(result)
         })
 
         app.get('/user/review/:id', async (req, res) => {
             const id = req.params.id
-            const query = { reviewId : id }
+            const query = { reviewId: id }
             const reviews = await reviewCollection.find(query).toArray()
+            res.send(reviews)
+        })
+        app.get('/user/review', async (req, res) => {
+            const reviews = await reviewCollection.find().toArray()
             res.send(reviews)
         })
 
 
+        // json webtoken part
 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.cookie('Token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+
+            res.send({ success: true })
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body
+            // console.log('loggihg out user', user)
+            res.clearCookie('Token', { maxAge: 0 }).send({ success: true })
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
