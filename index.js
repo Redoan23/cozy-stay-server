b85fd409baabb4ad3a20438027790e86b0e436f9
Redoan23 <redoan1080@gmail.com>
2024-05-14 04:08:12 +0600

    done

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 5000
 
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://cozy-stay-3af7a.web.app',
+        'https://cozy-stay-3af7a.firebaseapp.com'
     ],
     credentials: true
 }))
@@ -53,10 +55,17 @@ const client = new MongoClient(uri, {
     }
 });
 
+const cookieOption = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const roomCollection = client.db('hotelDB').collection('room-details')
         const usersBookedCollection = client.db('hotelDB').collection('booked-rooms-by-user')
@@ -168,11 +177,7 @@ async function run() {
             const user = req.body
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
-            res.cookie('Token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            })
+            res.cookie('Token', token, cookieOption)
 
             res.send({ success: true })
         })
@@ -180,11 +185,11 @@ async function run() {
         app.post('/logout', async (req, res) => {
             const user = req.body
             // console.log('loggihg out user', user)
-            res.clearCookie('Token', { maxAge: 0 }).send({ success: true })
+            res.clearCookie('Token', { ...cookieOption, maxAge: 0 }).send({ success: true })
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
